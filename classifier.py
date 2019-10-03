0c9f14a3a5915a8b53e57d89e4502965c406bfda
Mayank Shekhar (Prowareness) <m.shekhar@devon.nl>
2019-10-03 19:57:36 +0530

    Dataclassifier first class

--- classifier.py
+++ classifier.py
@@ -1,1 +1,12 @@
-#TEst
+from sklearn import tree
+
+#List of height, weight, shoe size
+BodyMeasurements=[[110,30,350], [140,20,490],[120,80,320],[140,80,400],[120,50,400],[141,50,470],[220,100,550],
+[210,60,250],[310,60,250],[210,70,550]]
+Gender=[['Male'],['Female'],['Male'],['Female'],['Male'],['Female'],['Male'],['Female'],['Male'],['Female']]
+
+#Now initiate a variable of tree.classifier class
+clf=tree.DecisionTreeClassifier()
+clf=clf.fit(BodyMeasurements,Gender)
+prediction=clf.predict([[110,32,78]])
+print (prediction)
